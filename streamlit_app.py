84c43e4d6eb0a3da6106f143180944fcfb40cfcf
Anjali Sinha <Andyy13@users.noreply.github.com>
2022-08-02 13:48:11 +0530

    Update streamlit_app.py
    added data in dataframe using text file

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.text('🥗Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🍉Build you own Fruit Smoothie🍒🍓')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+                                
